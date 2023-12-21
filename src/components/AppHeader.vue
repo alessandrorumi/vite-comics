8f84892fbea1e5b785ac13cc0ffa0dc5b0fdc3a7
alessandrorumi <rumi.alessandro21@gmail.com>
2023-12-21 15:10:41 +0100

    Add: AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,8 +6,55 @@ export default {
 
 <template>
   <header>
-    <h1>Header</h1>
+    <div class="logo">
+      <img src="../assets/img/dc-logo.png" alt="">
+    </div>
+    <nav>
+      <ul>
+        <li><a href="">CHARACTERS</a></li>
+        <li><a href="">COMICS</a></li>
+        <li><a href="">MOVIES</a></li>
+        <li><a href="">TV</a></li>
+        <li><a href="">GAMES</a></li>
+        <li><a href="">COLLECTIBLES</a></li>
+        <li><a href="">VIDEOS</a></li>
+        <li><a href="">FANS</a></li>
+        <li><a href="">NEWS</a></li>
+        <li><a href="">SHOP</a></li>
+      </ul>
+    </nav>
   </header>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+header {
+  height: 100px;
+  width: 100%;
+  display: flex;
+  justify-content: space-evenly;
+
+  .logo {
+    height: 100%;
+    display: flex;
+    align-items: center;
+
+    img {
+      height: 75%;
+    }
+  }
+
+  nav {
+    display: flex;
+    align-items: center;
+
+    ul {
+      display: flex;
+
+      li {
+        margin: 0 .75rem;
+      }
+    }
+  }
+
+}
+</style>
